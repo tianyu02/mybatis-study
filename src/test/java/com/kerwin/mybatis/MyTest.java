d874faf4ea302058c759df052935211fbc045573
tianyu <tianyu04@meicai.cn>
2019-09-22 11:09:35 +0800

    first commit

---
+++ src/test/java/com/kerwin/mybatis/MyTest.java
@@ -0,0 +1,12 @@
+package com.kerwin.mybatis;
+
+/**
+ * @author:tianyu
+ * @date: 2019/9/22
+ * @time: 11:08 AM
+ */
+public class MyTest {
+  public static void main(String[] args) {
+    System.out.println("kerwin is a good boy");
+  }
+}
